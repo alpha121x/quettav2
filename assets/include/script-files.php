42e980b3303d03dd5aed68ab2419077caac2ca6c
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-09-12 13:54:54 +0500

    Update script-files.php

--- assets/include/script-files.php
+++ assets/include/script-files.php
@@ -4,7 +4,7 @@
 <script src="assets/vendor/chart.js/chart.umd.js"></script>
 <script src="assets/vendor/echarts/echarts.min.js"></script>
 <script src="assets/vendor/quill/quill.min.js"></script>
-<!-- <script src="assets/vendor/simple-datatables/simple-datatables.js"></script> -->
+
 <script src="assets/vendor/tinymce/tinymce.min.js"></script>
 <script src="assets/vendor/php-email-form/validate.js"></script>
 
