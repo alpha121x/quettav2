b80a37f4a0e28183aff4e68ad1104137b8858f3e
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-09-13 13:38:46 +0500

    edited the modal

--- parcel_details.php
+++ parcel_details.php
@@ -89,7 +89,7 @@ require "DAL/db_config.php";
 
               <!-- Modal -->
               <div class="modal fade" id="viewModal" tabindex="-1" aria-labelledby="viewModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
                   <div class="modal-content">
                     <div class="modal-header">
                       <h5 class="modal-title" id="viewModalLabel">Parcel Details</h5>
